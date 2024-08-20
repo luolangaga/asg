fdcc288519a438b5a71bceda31cf122c9e1a7a00
王炜翔 <2307953404@qq.com>
2024-08-20 17:20:58 +0800

    ch

--- asg_form/Controllers/InviteReferee.cs
+++ asg_form/Controllers/InviteReferee.cs
@@ -1,7 +1,29 @@
-﻿namespace asg_form.Controllers
+﻿using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Mvc;
+
+namespace asg_form.Controllers
 {
-    public class InviteReferee
+    public class InviteReferee : ControllerBase
     {
+        private readonly RoleManager<Role> roleManager;
+        private readonly UserManager<User> userManager;
+        public InviteReferee(
+            RoleManager<Role> roleManager, UserManager<User> userManager)
+        {
+
+            this.roleManager = roleManager;
+            this.userManager = userManager;
+        }
+    }
+
+    [Route("api/v1/Invite")]
+    [HttpGet]
+    [Authorize]
+    public async Task<ActionResult<object>> GetReferee([FromBody] long Inviteeid)
+    {
+        string Invitorid = this.User.FindFirst(ClaimTypes.NameIdentifier)!.Value;
+        var user = await userManager.FindByIdAsync(Invitorid);
+
 
     }
 }
