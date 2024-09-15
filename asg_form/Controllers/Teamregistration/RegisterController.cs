7263b201c58d5c53afe552b82b5c3c6c428b1ba8
王炜翔 <2307953404@qq.com>
2024-09-15 21:56:59 +0800

    11

--- asg_form/Controllers/Teamregistration/RegisterController.cs
+++ asg_form/Controllers/Teamregistration/RegisterController.cs
@@ -22,6 +22,7 @@ namespace asg_form.Controllers.Teamregistration
 {
     public class ComformDB
     {
+        public long id { get; set; }
         public string chinaname { get; set; }
         public int user_id { get; set; }
         public int sex { get; set; }
@@ -187,6 +188,8 @@ namespace asg_form.Controllers.Teamregistration
 
                 var result = new
                 {
+                    code= 200,
+                    message="",
                     rows = Tasks,
                     total = TotalRecords,
                 };
